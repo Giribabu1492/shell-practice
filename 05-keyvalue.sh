88f08b40be91839a54b34114333e8590353bc4be
Giribabu1492 <jinagagiribabu@gmail.com>
2025-09-19 06:16:39 +0530

    shell files

--- 05-keyvalue.sh
+++ 05-keyvalue.sh
@@ -2,8 +2,15 @@
 
 declare -A myArray
 
-myArray =( [name]=Giribabu [age]=34 ) 
+myArray=([name]=Giribabu [age]=34 ) 
 
 echo "${myArray[name]}"
+echo "${myArray[age]}"
 
 
+declare -A mybaby
+
+mybaby=(name[shrihan] age[1])
+
+echo "${mybaby[name]}"
+ echo "${mybaby[age]}"
