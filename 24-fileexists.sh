889f6abfedfa05a8767378e64ecdf5b267ad6a14
Giribabu1492 <jinagagiribabu@gmail.com>
2025-09-19 15:57:55 +0530

    shell files

--- 24-fileexists.sh
+++ 24-fileexists.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 
-FILEPATH="/home/ec2-user/shell-practice/nawmes.txt"
+FILEPATH="/home/ec2-user/shell-practice/Giri.txt"
 
 if [ -f $FILEPATH ]
 then
  echo "exists"
  else 
+ touch $FILEPATH
  echo "not exists"
+
  fi
 
 
