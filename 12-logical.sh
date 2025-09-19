387c77027079f7ffb546ec8398e0be25b753888f
Giribabu1492 <jinagagiribabu@gmail.com>
2025-09-19 08:57:59 +0530

    shell files

---
+++ 12-logical.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+read -r -p "whatis your age" age
+read -r -p "whatis your country" country
+
+    if [ $age -ge 18 ] && [ "$country" = "india" ]
+    then
+    echo "you can vote"
+    else 
+    echo "you can not"
+    fi
